b5089b638b8f717158a0b02abfd67fe3152c41e4
DmitryZ <zagarskih@inbox.ru>
2022-06-25 15:56:08 +0500

    new branch function

--- examples001_9max/Program.cs
+++ examples001_9max/Program.cs
@@ -1,4 +1,12 @@
-﻿int a1 = new Random().Next(1,999);
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result
+}
+
+int a1 = new Random().Next(1,999);
 Console.WriteLine (a1);
 int b1 = new Random().Next(1,999);
 Console.WriteLine (b1);
@@ -16,3 +24,17 @@ int b3 = new Random().Next(1,999);
 Console.WriteLine (b3);
 int c3 = new Random().Next(1,999);
 Console.WriteLine (c3);
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine (max);
