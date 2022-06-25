92e24758f2b0143a78d3aa2f5ccb51c44acaca63
DmitryZ <zagarskih@inbox.ru>
2022-06-25 16:11:43 +0500

    functiom Max ready

--- examples001_9max/Program.cs
+++ examples001_9max/Program.cs
@@ -3,10 +3,11 @@
     int result = arg1;
     if (arg2>result) result = arg2;
     if (arg3>result) result = arg3;
-    return result
+    return result;
 }
 
 int a1 = new Random().Next(1,999);
+Console.Write ("a1 = ");
 Console.WriteLine (a1);
 int b1 = new Random().Next(1,999);
 Console.WriteLine (b1);
@@ -25,16 +26,17 @@ Console.WriteLine (b3);
 int c3 = new Random().Next(1,999);
 Console.WriteLine (c3);
 
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
+// int max1 = Max(a1,b1,c1);
+// int max2 = Max(a2,b2,c2);
+// int max3 = Max(a3,b3,c3);
+// int max = Max(max1,max2,max3);
 
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c2 > max) max = c2;
-
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
+int max = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a2,b2,c2));
 
 Console.WriteLine (max);
+// Console.WriteLine (max2);
+// Console.WriteLine (max3);
+// Console.WriteLine (max);
